5995b03fae3eaf9863803d5f813d1b8b59f6a5f7
CmlMagneville <camille@magneville.net>
2021-02-04 13:54:39 +0100

    Change sp_tr data nms :package:

---
+++ R/sp_tr_fruits.R
@@ -0,0 +1,12 @@
+#' @title Species traits dataframe linking fruits and traits for mFD use
+#' @description A dataframe linking 25 fruits species (rows) and their traits
+#'   values for 6 functional traits (columns). It is used in all mFD tutorials
+#'   and diverse mFD functions examples
+#'   
+#' @format A dataframe with 25 rows and 8 columns
+#' \describe{
+#'   \item{sp}{25 fruits species}
+#'   \item{tr}{6 traits of different types}
+#' }
+#'
+"sp_tr_fruits"
