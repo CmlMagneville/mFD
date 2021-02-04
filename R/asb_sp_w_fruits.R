73d492cab49e299ff9c74f61b8fa51d3182b5861
CmlMagneville <camille@magneville.net>
2021-02-04 14:03:35 +0100

    Create asb_sp_w mFD data :package:

---
+++ R/asb_sp_w_fruits.R
@@ -0,0 +1,12 @@
+#' @title Species assemblages matrix for fruits baskets example
+#' @description A matrix gathering 25 fruits species into 10 fruits baskets.
+#'   Each number in the matrix is fruits abundance. This matrix is used in most
+#'   mFD tutorials and diverse mFD functions examples.
+#'   
+#' @format A matrix with 10 rows and 25 columns
+#' \describe{
+#'   \item{asb}{10 fruits baskets}
+#'   \item{sp}{25 fruits species}
+#' }
+#'
+"asb_sp_w_fruits"
