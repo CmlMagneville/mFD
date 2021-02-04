5833fa4801a640ead2a9ef4311104ad66a4aaa64
CmlMagneville <camille@magneville.net>
2021-02-04 14:15:28 +0100

    Add sp_tr_cat mFD data :package:

---
+++ R/sp_tr_cat_fruits.R
@@ -0,0 +1,13 @@
+#' @title Dataframe of traits category for fruits example
+#' @description A dataframe summarizing traits types for each trait of the
+#'   fruits baskets dataset. First column gathers traits names, second columns
+#'   gathers traits types, third column summarizes to which fuzzy trait belongs
+#'   each column that refers to a fuzzy trait.
+#'   
+#' @format A dataframe with 8 rows and 3 columns
+#' \describe{
+#'   \item{tr}{6 traits of different types}
+#'   \item{tr_cat}{traits names, traits types and names for fuzzy coded traits}
+#' }
+#'
+"sp_tr_cat_fruits"
