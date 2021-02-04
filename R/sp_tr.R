13a364e03dfa4f8dd6ae9fa58d5fd76932cba6f9
CmlMagneville <camille@magneville.net>
2021-02-04 13:41:10 +0100

    Add sp_tr to mFD data :package:

---
+++ R/sp_tr.R
@@ -0,0 +1,11 @@
+#' @title Species traits dataframe for the fruits baskets example
+#' @description A dataframe gathering 25 fruits species (rows) and values for 6
+#'   traits (columns) used in all mFD tutorials and divers functions examples
+#'
+#' @format dataframe with 25 rows and 8 columns
+#' \describe{
+#'   \item{sp}{25 fruits species}
+#'   \item{tr}{6 traits}
+#' }
+#'
+"sp_tr"
