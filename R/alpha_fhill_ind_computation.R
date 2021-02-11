a15f8726fc539ced951d0a1c79787ffd84642638
CmlMagneville <camille@magneville.net>
2021-02-11 16:21:12 +0100

    Format alpha.fd.hill :lipstick:

--- R/alpha_fhill_ind_computation.R
+++ R/alpha_fhill_ind_computation.R
@@ -1,13 +1,4 @@
-# ------------------------------------------------------------------------------
-# Function to compute  functional alpha diversity indices based on Hill numbers
-# applied to distance between species
-#
-# Authors: Sébastien Villéger and Camille Magneville
-#
-# ------------------------------------------------------------------------------
-
-
-#'Compute functional alpha diversity indices based on Hill numbers
+#' Compute functional alpha diversity indices based on Hill numbers
 #'
 #' Compute functional alpha diversity applied to distance between species
 #' following the framework from Chao et al.2019, Ecological Monographs
@@ -15,28 +6,28 @@
 #' where function 'f' in equation 3c is linear:f(dij(tau)) = dij(tau)/tau, hence
 #' f(0) = 0 and f(tau) = 1.
 #'
-#'@param asb_sp_w a \strong{matrix} with weight of species (columns) in a set of
+#' @param asb_sp_w a \strong{matrix} with weight of species (columns) in a set of
 #'  assemblages (rows). Rows and columns should have names. NA are not allowed.
 #'
-#'@param sp_dist a \strong{matrix or dist object} with distance between species. Species
-#' names should be provided and match those in 'asb_sp_w'. NA are not allowed.
+#' @param sp_dist a \strong{matrix or dist object} with distance between species. Species
+#'  names should be provided and match those in 'asb_sp_w'. NA are not allowed.
 #'
-#'@param q a \strong{vector} containing values referring to the order of diversity to use
+#' @param q a \strong{vector} containing values referring to the order of diversity to use
 #'
-#'@param tau a \strong{character string} with name of function to apply to distance
-#' matrix (i.e. among all pairs of species) to get the threshold used to define
-#' 'functionally indistinct set of species'. Could be qet to 'mean' (default),
-#' 'min' or 'max'.
+#' @param tau a \strong{character string} with name of function to apply to distance
+#'  matrix (i.e. among all pairs of species) to get the threshold used to define
+#'  'functionally indistinct set of species'. Could be qet to 'mean' (default),
+#'  'min' or 'max'.
 #'
-#'@param check_input a \strong{logical value} defining whether inputs are checked before
+#' @param check_input a \strong{logical value} defining whether inputs are checked before
 #'  computation of indices. Possible error messages will thus may be more
 #'  understandable for the user than R error messages. Default: check_input =
 #'  TRUE.
 #'
-#'@param details_returned a \strong{logical value} indicating whether the user want to store
-#' values used for computing indices (see list below)
+#' @param details_returned a \strong{logical value} indicating whether the user want to store
+#'  values used for computing indices (see list below)
 #'
-#'@return a list with: \itemize{
+#' @return a list with: \itemize{
 #'
 #'  \item \emph{asb_FD_Hill} a matrix containing indices values for each level of
 #'  q (columns, named as 'FD_qx') for each assemblage (rows, named as in
@@ -51,41 +42,40 @@
 #'  assemblages
 #'  }
 #'  }
+#'  
+#' @author Sébastien Villéger
 #'
-#'@note FD computed with q=2 and tau = 'max' is equivalent to the Rao's quadratic
-#' entropy from Ricotta & Szeidl (2009, J Theor Biol).
-#' FD computed with tau = 'min' is equivalent to Hill number taxonomic diversity,
-#' thus with q=0 it is species richness (S), with q = 1 it is exponential of Shannon
-#' entropy (H) and with q = 2 it is 1/(1-D) where D is Simpson diversity
-#' FD is computed applying the special case where function 'f' in equation 3c
-#' is linear:f(dij(tau)) = dij(tau)/tau, hence f(0)=0 and f(tau)=1.
+#' @note FD computed with q=2 and tau = 'max' is equivalent to the Rao's quadratic
+#'  entropy from Ricotta & Szeidl (2009, J Theor Biol).
+#'  FD computed with tau = 'min' is equivalent to Hill number taxonomic diversity,
+#'  thus with q=0 it is species richness (S), with q = 1 it is exponential of Shannon
+#'  entropy (H) and with q = 2 it is 1/(1-D) where D is Simpson diversity
+#'  FD is computed applying the special case where function 'f' in equation 3c
+#'  is linear:f(dij(tau)) = dij(tau)/tau, hence f(0)=0 and f(tau)=1.
 #'
-#'@examples
+#' @examples
 #' # Load Species*Traits dataframe:
-#' data("fruits_traits", package = "mFD")
+#' data('fruits_traits', package = 'mFD')
 #' # Load Assemblages*Species dataframe:      
-#' data("baskets_fruits_weights", package = "mFD")   
+#' data('baskets_fruits_weights', package = 'mFD')   
 #' # Compute functional distance 
 #' sp_dist_fruits <- mFD::funct.dist(sp_tr = fruits_traits,         
 #'  tr_cat       = fruits_traits_cat,   
-#'  dist_metric  = "kgower",         
-#'  scaling      = "scaledBYrange",  
+#'  dist_metric  = 'kgower',         
+#'  scaling      = 'scaledBYrange',  
 #'  stop_if_NA   = TRUE)
 #' # Compute alpha fd hill indices:
 #' alpha.fd.hill(asb_sp_w = baskets_fruits_weights, sp_dist = sp_dist_fruits, q = c(0, 1, 2),
-#'  tau = "mean", check_input = TRUE, details_returned = TRUE)
+#'  tau = 'mean', check_input = TRUE, details_returned = TRUE)
 #'
 #'@export
 
-alpha.fd.hill <- function(asb_sp_w,
-                          sp_dist,
-                          q = c(0, 1, 2),
-                          tau = "mean",
-                          check_input = TRUE,
+alpha.fd.hill <- function(asb_sp_w, sp_dist, 
+                          q = c(0, 1, 2), tau = "mean", check_input = TRUE, 
                           details_returned = TRUE) {
   
   
-  ####  distance between species stored in a matrix  ####
+  #### distance between species stored in a matrix ####
   
   sp_sp_dist <- sp_dist
   
@@ -125,30 +115,32 @@ alpha.fd.hill <- function(asb_sp_w,
       stop("Error: The 'asp_sp_w' dataframe must only contain numeric values. Please convert values")
     }
     
-    if (any(! (colnames(asb_sp_w) %in% rownames(sp_sp_dist) ) ) ) {
+    if (any(!(colnames(asb_sp_w) %in% rownames(sp_sp_dist)))) {
       stop(paste("Error: Mismatch between names in species*weight and
                    species distances matrix. Please check."))
     }
     
-    if(any(! q %in% c(0,1,2) ) ) {
+    if (any(!q %in% c(0, 1, 2))) {
       stop(paste("Error: q should be 0, 1 and/or 2.
                   Please check."))
     }
     
-    if(any(! tau %in% c("min", "mean", "max") ) ) {
+    if (any(!tau %in% c("min", "mean", "max"))) {
       stop(paste("Error: tau should be 'min', 'mean' or 'max'. Please check."))
     }
     
-    # Add a stop if some species do not belong to any assemblage:
-    if (min(apply(asb_sp_w, 2, sum)) == 0){
+    # Add a stop if some species do not belong to any
+    # assemblage:
+    if (min(apply(asb_sp_w, 2, sum)) == 0) {
       stop("Error: Some species are absent from all assemblages.")
     }
     # Add a stop if some asb do not contain species:
-    if (min(apply(asb_sp_w, 1, sum)) == 0){
+    if (min(apply(asb_sp_w, 1, sum)) == 0) {
       stop("Error: Some assemblages do not contain species.")
     }
     
-    # Add a stop if there is a negative value in the occurrence dataframe:
+    # Add a stop if there is a negative value in the
+    # occurrence dataframe:
     if (any(asb_sp_w < 0)) {
       stop("Error: The species*weight dataframe should not contain negative values.
            Please check.")
@@ -156,36 +148,38 @@ alpha.fd.hill <- function(asb_sp_w,
     
   }
   
-  ####  preliminary operations ####
+  #### preliminary operations ####
   
-  # ensuring species are in the same order in both matrices:
+  # ensuring species are in the same order in both
+  # matrices:
   asb_sp_w <- as.matrix(asb_sp_w)
   sp_sp_dist <- sp_sp_dist[colnames(asb_sp_w), colnames(asb_sp_w)]
   
   
-  # computing total weight per assemblage and relative weights of species ----
+  # computing total weight per assemblage and
+  # relative weights of species ----
   asb_totw <- apply(asb_sp_w, 1, sum)
   asb_sp_relw <- asb_sp_w/asb_totw
   
   # computing tau as mean or max on distances ----
   tau_dist <- NULL
   
-  if( tau == "min") {
+  if (tau == "min") {
     tau_dist <- min(sp_dist)
     
     # special case of null distance outside diagonal
     if (tau_dist == 0) {
-      tau_dist<- min(sp_dist[sp_dist != 0])
+      tau_dist <- min(sp_dist[sp_dist != 0])
       cat("Warning: some species has null functional distance,
           'tau' was set to the minimum non-null distance")
     }
   }
   
-  if( tau == "mean") {
+  if (tau == "mean") {
     tau_dist <- mean(sp_dist)
   }
   
-  if( tau == "max") {
+  if (tau == "max") {
     tau_dist <- max(sp_dist)
   }
   
@@ -194,28 +188,33 @@ alpha.fd.hill <- function(asb_sp_w,
   dij_tau[which(dij_tau > tau_dist, arr.ind = T)] <- tau_dist
   
   
-  ####  computing diversity of assemblages ####
+  #### computing diversity of assemblages ####
   
   # empty matrix to store outputs
-  asb_FD_Hill <- matrix(NA, nrow(asb_sp_w), length(q),
-                        dimnames = list(row.names(asb_sp_w), paste0("FD_q", q)))
+  asb_FD_Hill <- matrix(NA, nrow(asb_sp_w), length(q), 
+                        dimnames = list(row.names(asb_sp_w), paste0("FD_q", 
+                                                                    q)))
   
-  # loop on assemblages (equations id refers to those in Chao et al 2019
+  # loop on assemblages (equations id refers to those
+  # in Chao et al 2019
   for (k in row.names(asb_sp_w)) {
     
     # total weight (n+ in eq 4a)
     nplus_k <- asb_totw[k]
     
     # species names present in assemblage k
-    sp_k <- colnames(asb_sp_w)[which(asb_sp_w[k, ] > 0)]
+    sp_k <- colnames(asb_sp_w)[which(asb_sp_w[k, 
+    ] > 0)]
     
-    # f(dij(tau)) with f being linear so dij(tau)/tau (see bottom right of p7)
+    # f(dij(tau)) with f being linear so dij(tau)/tau
+    # (see bottom right of p7)
     f_dij_tau <- dij_tau[sp_k, sp_k]/tau_dist
     
     # 'abundance of species given tau' (eq 3c)
     a_k <- (1 - f_dij_tau) %*% asb_sp_w[k, sp_k]
     
-    # attribute contribution of species given tau (eq 3d)
+    # attribute contribution of species given tau (eq
+    # 3d)
     v_k <- asb_sp_w[k, sp_k]/a_k[, 1]
     
     # diversity of order 0 (eq 4c)
@@ -225,16 +224,18 @@ alpha.fd.hill <- function(asb_sp_w,
     
     # diversity of order 1 (eq 4d)
     if (1 %in% q) {
-      asb_FD_Hill[k, "FD_q1"] <- exp(sum(- v_k*a_k/nplus_k*log(a_k/nplus_k)))
+      asb_FD_Hill[k, "FD_q1"] <- exp(sum(-v_k * 
+                                           a_k/nplus_k * log(a_k/nplus_k)))
     }
     
     # diversity of order 2 (eq 4e)
     if (2 %in% q) {
-      asb_FD_Hill[k, "FD_q2"] <- 1 / (sum(v_k*(a_k/nplus_k)^2))
+      asb_FD_Hill[k, "FD_q2"] <- 1/(sum(v_k * 
+                                          (a_k/nplus_k)^2))
     }
     
     
-  }# end of k
+  }  # end of k
   
   
   #### outputs ####
@@ -244,15 +245,12 @@ alpha.fd.hill <- function(asb_sp_w,
   
   # details if required
   if (details_returned == TRUE) {
-    res <- list(asb_FD_Hill = asb_FD_Hill,
-                tau_dist = tau_dist,
-                details= list( asb_totw = asb_totw,
-                               asb_sp_relw = asb_sp_relw)
-    )
+    res <- list(asb_FD_Hill = asb_FD_Hill, tau_dist = tau_dist, 
+                details = list(asb_totw = asb_totw, asb_sp_relw = asb_sp_relw))
   }
   
   # returning
   return(res)
   
   
-} # end of function
+}  # end of function
