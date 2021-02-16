e7d271422dd5fccd4568406abcae83569244ea6b
CmlMagneville <camille@magneville.net>
2021-02-16 13:54:13 +0100

    Format sp.filter :lipstick:

--- R/sp_filtering.R
+++ R/sp_filtering.R
@@ -1,12 +1,3 @@
-# Function to filter species by community
-#
-# Authors: Camille Magneville & Sébastien Villéger
-#
-#
-
-# ------------------------------------------------------------------------------
-
-
 #' Retrieve information about species in a given assemblage
 #'
 #' This function computes names of species present in an given assemblage, their
@@ -14,52 +5,61 @@
 #' \code{alpha_FD_multidim} function to filter species and compute each
 #' functional indices for each community.
 #'
-#' @param asb_nm a \strong{string} object referring to the name of a given
-#' community.
+#' @param asb_nm a string object referring to the name of a given community.
 #'
-#' @param sp_faxes_coord a \strong{matrix} of species coordinates in a chosen functional
+#' @param sp_faxes_coord a matrix of species coordinates in a chosen functional
 #'   space. Species coordinates have been retrieved thanks to
 #'   \code{\link{tr.cont.fspace}} or \code{\link{quality.fspaces}}.
 #'
-#' @param asb_sp_w a \strong{matrix} linking weight of species (columns) and a set of
-#'   assemblages (rows).
+#' @param asb_sp_w a \strong{matrix} linking weight of species (columns) and a
+#'   set of assemblages (rows).
 #'
-#' @return a vector containing names of species present in a given assemblage \code{sp_name_asb_k}, a
-#'   matrix containing coordinates of species present in a given assemblage \code{sp_faxes_coord_k}, a
-#'   matrix containing weight of species present in a given assemblage \code{asb_sp_w_k}, a matrix
-#'   containing relative weight of species present in a given assemblage \code{asb_sp_relatw_k}.
+#' @return a vector containing names of species present in a given assemblage
+#'   \code{sp_name_asb_k}, a matrix containing coordinates of species present in
+#'   a given assemblage \code{sp_faxes_coord_k}, a matrix containing weight of
+#'   species present in a given assemblage \code{asb_sp_w_k}, a matrix
+#'   containing relative weight of species present in a given assemblage
+#'   \code{asb_sp_relatw_k}.
 #'
 #' @examples
 #' # Load Species*Traits dataframe:
-#' data("fruits_traits", package = "mFD")
+#'  data("fruits_traits", package = "mFD")
 #' 
 #' # Load Assemblages*Species dataframe:      
-#' data("baskets_fruits_weights", package = "mFD")
+#'  data("baskets_fruits_weights", package = "mFD")
 #'  
 #' # Load Traits categories dataframe:
-#' data("fruits_traits_cat", package = "mFD")  
+#'  data("fruits_traits_cat", package = "mFD")  
 #' 
 #' # Compute functional distance 
-#' sp_dist_fruits <- mFD::funct.dist(sp_tr  = fruits_traits,
-#'  tr_cat = fruits_traits_cat,
-#'  metric = "gower",
+#' sp_dist_fruits <- mFD::funct.dist(
+#'  sp_tr         = fruits_traits,
+#'  tr_cat        = fruits_traits_cat,
+#'  metric        = "gower",
 #'  scale_euclid  = "scale_center",
-#'  ordinal_var = "classic",
-#'  weight_type = "equal",
-#'  stop_if_NA  = TRUE)
+#'  ordinal_var   = "classic",
+#'  weight_type   = "equal",
+#'  stop_if_NA    = TRUE)
 #'  
 #' # Compute functional spaces quality to retrieve species coordinates matrix:
-#' fspaces_quality_fruits <- mFD::quality.fspaces(sp_dist = sp_dist_fruits, 
+#' fspaces_quality_fruits <- mFD::quality.fspaces(
+#'  sp_dist             = sp_dist_fruits, 
 #'  maxdim_pcoa         = 10,
 #'  deviation_weighting = "absolute",
 #'  fdist_scaling       = FALSE,
 #'  fdendro             = "average")
 #'  
 #' # Retrieve species coordinates matrix:
-#' sp_faxes_coord_fruits <- fspaces_quality_fruits$"details_fspaces"$"sp_pc_coord"
+#'  sp_faxes_coord_fruits <- 
+#'  fspaces_quality_fruits$"details_fspaces"$"sp_pc_coord"
 #' 
 #' # Filter species of basket_1 assemblage:
-#' sp.filter(asb_nm = "basket_1", sp_faxes_coord_fruits, baskets_fruits_weights)
+#'  sp.filter(
+#'  asb_nm         = "basket_1", 
+#'  sp_faxes_coord = sp_faxes_coord_fruits, 
+#'  asb_sp_w       = baskets_fruits_weights)
+#' 
+#' @author Camille Magneville and Sébastien Villéger
 #'
 #'@export
 
